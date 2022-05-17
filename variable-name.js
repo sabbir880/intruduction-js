c4a47835f5c9c80363108badab36f8ac457064cf
sabbir880 <drsabbir880@gmail.com>
2022-05-18 00:44:46 +0600

    variable-name

---
+++ variable-name.js
@@ -0,0 +1,12 @@
+var name = "Hero Alam";
+
+// reserve keyword not allow
+
+// name should be one word 
+ var name = ' nur vai';
+
+//  quote is not allow for name 
+
+// long name 
+ var roomMateName = 'abdullah';
+
--- varibales.js
+++ varibales.js
@@ -11,3 +11,6 @@ var name = 'Sabbir';
 
 var pass = true;
 var fail = false;
+
+// o variable tor nam dibo ki vabhi sudu tai ami 
+
