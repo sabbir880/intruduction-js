d7872cc8b30f6bf83920b4b9f176207f2b51335c
sabbir880 <drsabbir880@gmail.com>
2022-05-24 23:32:12 +0600

    var name

--- variable-name.js
+++ variable-name.js
@@ -4,9 +4,12 @@ var name = "Hero Alam";
 
 // name should be one word 
  var name = ' nur vai';
+ var friend = 'atik vai';
 
 //  quote is not allow for name 
 
 // long name 
  var roomMateName = 'abdullah';
+ var schoolName = ' Ideal High school';
+ var PIE = 3.14;
 
