449e4b111f0509f81f5b17435967c10fa3938ad9
sabbir880 <drsabbir880@gmail.com>
2022-05-27 23:53:20 +0600

    conversion

---
+++ conversation.js
@@ -0,0 +1,26 @@
+var firstName = 'Sabbir';
+var lastName = 'Rahman';
+
+var fullName = firstName + ' '+ lastName;
+console.log(fullName)
+
+// integer number 
+var penPrice = "10";
+var pepperPrice = '20';
+// var total = penPrice + pepperPrice;
+var penPriceNumber = parseInt(penPrice);
+var pepperPriceNumber = parseInt(pepperPrice)
+var total = penPriceNumber + pepperPriceNumber;
+
+console.log(total)
+
+// float number 
+
+var onionPrice = "15.50";
+var potatoPrice = "20.25";
+// console.log(onionPrice+potatoPrice)
+var onionPriceNumber = parseFloat(onionPrice);
+var potatoPriceNumber = parseFloat(potatoPrice);
+
+var totalPrice = onionPriceNumber + potatoPriceNumber;
+console.log(totalPrice)
